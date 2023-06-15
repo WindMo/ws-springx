055795126d9c58682f0d680de8a4a6e350293065
WindMo <1459328339@qq.com>
2023-06-15 20:33:04 +0800

    init project

---
+++ ws-springx-mybatis-test/src/main/java/ws/mybatis/spring/WsMybatisSpringExtendApplication.java
@@ -0,0 +1,13 @@
+package ws.mybatis.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsMybatisSpringExtendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsMybatisSpringExtendApplication.class, args);
+    }
+
+}
