3cf1914139b3510d9a66f30bd1eb32ec746c0a12
WindMo <1459328339@qq.com>
2024-02-29 01:56:49 +0800

    test: FrequencyRestrict 频控器

---
+++ ws-springx-common-test/src/test/java/ws/spring/testdemo/restrict/SimpleFrequencyRestrictServiceTests.java
@@ -0,0 +1,16 @@
+package ws.spring.testdemo.restrict;
+
+import ws.spring.restrict.FrequencyRestrictService;
+import ws.spring.restrict.support.SimpleFrequencyRestrictService;
+
+/**
+ * @author WindShadow
+ * @version 2024-02-24.
+ */
+public class SimpleFrequencyRestrictServiceTests extends FrequencyRestrictServiceTests {
+
+    @Override
+    protected FrequencyRestrictService createFrequencyRestrictService() {
+        return new SimpleFrequencyRestrictService();
+    }
+}
