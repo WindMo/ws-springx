90d0b130e8ec843694f311fa4baa6a128a3c4efd
WindMo <1459328339@qq.com>
2024-02-29 01:55:16 +0800

    feat: FrequencyRestrict 频控器

---
+++ ws-springx-common/src/main/java/ws/spring/restrict/annotation/FrequencyRestrict.java
@@ -0,0 +1,45 @@
+package ws.spring.restrict.annotation;
+
+import org.springframework.core.annotation.AliasFor;
+import ws.spring.restrict.RestrictedCriticalException;
+
+import java.lang.annotation.*;
+
+/**
+ * 声明式频控注解，用于控制方法的调用频率。超过调用频率将以{@linkplain RestrictedCriticalException 临界异常}通知
+ *
+ * @author WindShadow
+ * @version 2023-10-18.
+ * @see ws.spring.restrict.FrequencyRestrictor
+ * @see ws.spring.restrict.FrequencyRestrictService
+ */
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface FrequencyRestrict {
+
+    /**
+     * 使用的频控器名称
+     */
+    String name() default "";
+
+    @AliasFor("frequency")
+    int value() default 1;
+
+    /**
+     * 频控依据spel表达式，
+     * 表达式结果通常其不应该为null，这取决于使用的 FrequencyRestrictService是否支持null值的refer
+     */
+    String refer() default "";
+
+    /**
+     * 频次，正整数
+     */
+    int frequency() default 1;
+
+    /**
+     * 持续时长（单位：秒），默认1秒，最低为1秒
+     */
+    long duration() default 1L;
+}
---
+++ ws-springx-common/src/main/java/ws/spring/restrict/support/SimpleFrequencyRestrictService.java
@@ -0,0 +1,70 @@
+package ws.spring.restrict.support;
+
+import ws.spring.restrict.FrequencyRestrictor;
+import ws.spring.restrict.FrequencyRestrictorDefinition;
+import ws.spring.restrict.RestrictorDeclarationException;
+
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author WindShadow
+ * @version 2024-01-26.
+ */
+
+public class SimpleFrequencyRestrictService extends AbstractFrequencyRestrictService {
+
+    private final Map<String, FrequencyRestrictor> restrictors = new ConcurrentHashMap<>(16);
+
+    @Override
+    protected FrequencyRestrictor createRestrictor(FrequencyRestrictorDefinition definition) throws RestrictorDeclarationException {
+        return new BlockingFrequencyRestrictor(definition);
+    }
+
+    @Override
+    protected boolean hasRestrictor(String restrictorName) {
+        return restrictors.containsKey(restrictorName);
+    }
+
+    @Override
+    protected void doAddRestrictor(FrequencyRestrictor restrictor) {
+        restrictors.putIfAbsent(restrictor.getName(), restrictor);
+    }
+
+    @Override
+    public FrequencyRestrictor getRestrictor(String restrictorName) {
+        return restrictors.get(restrictorName);
+    }
+
+    protected static class BlockingFrequencyRestrictor extends AbstractFrequencyRestrictor {
+
+        private final int frequency;
+        private final long durationMillis;
+
+        /**
+         * key为refer，队列元素为毫秒时间戳
+         */
+        private final Map<String, BlockingQueue<Long>> referQueues;
+
+        protected BlockingFrequencyRestrictor(FrequencyRestrictorDefinition definition) {
+            super(definition.getName(), false);
+            this.frequency = definition.getFrequency();
+            this.durationMillis = TimeUnit.SECONDS.toMillis(definition.getDuration());
+            this.referQueues = new ConcurrentHashMap<>();
+        }
+
+        @Override
+        protected boolean doTryRestrict(String refer) {
+
+            BlockingQueue<Long> queue = referQueues.computeIfAbsent(refer, r -> new ArrayBlockingQueue<>(frequency));
+            long currentTimeMillis = System.currentTimeMillis();
+            if (queue.offer(currentTimeMillis)) return false;
+            // 如果队列已满，则尝试移除旧时间戳
+            while (!queue.isEmpty() && (queue.peek() + durationMillis <= currentTimeMillis)) queue.remove();
+            return !queue.offer(currentTimeMillis);
+        }
+    }
+}
