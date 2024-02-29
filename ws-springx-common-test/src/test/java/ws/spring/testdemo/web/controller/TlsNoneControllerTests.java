2e9b7b730e74d7795c0901bc642697c5903f6e8d
WindMo <1459328339@qq.com>
2024-02-29 23:32:04 +0800

    test: Tls配置支持

---
+++ ws-springx-common-test/src/test/java/ws/spring/testdemo/web/controller/TlsNoneControllerTests.java
@@ -0,0 +1,21 @@
+package ws.spring.testdemo.web.controller;
+
+import org.springframework.boot.web.server.Ssl;
+import org.springframework.test.context.ActiveProfiles;
+import ws.spring.testdemo.util.HttpClientAssistants;
+
+/**
+ * @author WindShadow
+ * @version 2022-10-02.
+ */
+@ActiveProfiles({"ssl_none"})
+public class TlsNoneControllerTests extends BaseTlsControllerTests {
+
+    @Override
+    protected void doTlsTest(boolean tls, boolean need, Ssl ssl) {
+
+        doHttpTest(tls, true, HttpClientAssistants.newUnidirectionalAuthRestTemplate(ssl.getTrustStore(), ssl.getTrustStorePassword().toCharArray()));
+        doHttpTest(tls, true, HttpClientAssistants.newIgnoreAuthServerRestTemplate(ssl.getKeyStore(), ssl.getKeyStorePassword().toCharArray(), ssl.getKeyPassword().toCharArray()));
+        doHttpTest(tls, true, HttpClientAssistants.newIgnoreAuthServerRestTemplate());
+    }
+}
