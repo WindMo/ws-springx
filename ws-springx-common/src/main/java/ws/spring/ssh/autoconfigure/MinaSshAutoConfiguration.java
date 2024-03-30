cbd69c810c51132fa4281f2ef6965d569da9e467
WindMo <1459328339@qq.com>
2024-03-31 00:55:20 +0800

    feat: ssh操作基本模板与自动装配

---
+++ ws-springx-common/src/main/java/ws/spring/ssh/autoconfigure/MinaSshAutoConfiguration.java
@@ -0,0 +1,31 @@
+package ws.spring.ssh.autoconfigure;
+
+import org.apache.sshd.client.SshClient;
+import org.apache.sshd.client.keyverifier.AcceptAllServerKeyVerifier;
+import org.apache.sshd.client.session.ClientSessionCreator;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+
+
+@AutoConfiguration
+@ConditionalOnClass(SshClient.class)
+@AutoConfigureBefore(SshAutoConfiguration.class)
+@ConditionalOnMissingBean(SshServiceFactory.class)
+public class MinaSshAutoConfiguration {
+
+    @Bean
+    public static SshServiceFactory sshServiceFactory(ClientSessionCreator client) {
+        return new MinaSshServiceFactory(client);
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "close")
+    public static SshClient defaultClient() {
+
+        SshClient client = SshClient.setUpDefaultClient();
+        client.setServerKeyVerifier(AcceptAllServerKeyVerifier.INSTANCE);
+        return client;
+    }
+}
