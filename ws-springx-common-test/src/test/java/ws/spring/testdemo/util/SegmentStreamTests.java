14478d3e438163b2da669807bf8f5ae0b7c8da2a
WindMo <1459328339@qq.com>
2025-05-30 00:53:08 +0800

    feat: SegmentStream分段流

---
+++ ws-springx-common-test/src/test/java/ws/spring/testdemo/util/SegmentStreamTests.java
@@ -0,0 +1,131 @@
+package ws.spring.testdemo.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ws.spring.util.SegmentStream;
+import ws.spring.util.SegmentStreams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+/**
+ * @author WindShadow
+ * @version 2025-05-29.
+ * @see SegmentStream
+ * @see SegmentStreams
+ */
+public class SegmentStreamTests {
+
+    protected final Random random = new Random();
+
+    private List<Object> data;
+    private SegmentStream<Object> stream;
+
+    @BeforeEach
+    void before() {
+
+        data = new ArrayList<>();
+        int length = random.nextInt(100) + 1;
+        for (int i = 1; i <= length; i++) {
+            data.add(i);
+        }
+        int interval = (int) Math.ceil(length / 20.0);
+        stream = SegmentStreams.ofCollection(interval, data);
+    }
+
+    @Test
+    void forEachTest() {
+
+        final int interval = stream.getInterval();
+
+        List<Object> list = new ArrayList<>(data.size());
+        int count = 0;
+
+        while (count < 2) {
+
+            count++;
+            Assertions.assertSame(interval, stream.getInterval());
+            Assertions.assertSame(0, stream.getOffset());
+
+            stream.forEach((Consumer<Object>) list::add);
+            Assertions.assertEquals(data, list);
+            Assertions.assertSame(list.size(), stream.getOffset());
+
+            stream.reset();
+            list.clear();
+        }
+
+        while (count < 4) {
+
+            count++;
+            stream.forEachSegment((Consumer<List<Object>>) list::addAll);
+            Assertions.assertEquals(data, list);
+            Assertions.assertSame(list.size(), stream.getOffset());
+
+            stream.reset();
+            list.clear();
+        }
+
+        int maxSize = data.size() / 2;
+        stream.forEach(v -> {
+
+            list.add(v);
+            return list.size() < maxSize;
+        });
+        Assertions.assertSame(maxSize, list.size());
+        Assertions.assertTrue(list.size() <= stream.getOffset());
+        Assertions.assertEquals(data.stream().limit(maxSize).collect(Collectors.toList()), list);
+
+        stream.reset();
+        list.clear();
+
+        int maxSegment = Math.max(data.size() / interval - 1, 1);
+        stream.forEachSegment(vs -> {
+
+            list.addAll(vs);
+            return Math.ceil(list.size() / (interval + 0.0)) < maxSegment;
+        });
+        Assertions.assertSame(Math.min(maxSegment * interval, data.size()), list.size());
+        Assertions.assertEquals(data.stream().limit(list.size()).collect(Collectors.toList()), list);
+    }
+
+    @Test
+    void mapTest() {
+
+        data = data.stream().map(Object::toString).collect(Collectors.toList());
+        stream = stream.map(Object::toString);
+        forEachTest();
+    }
+
+    @Test
+    void emptyTest() {
+
+        int interval = random.nextInt(10) + 1;
+        SegmentStream<Object> emptyStream = SegmentStreams.emptySegmentStream(interval);
+        int count = 0;
+        while (count < 3) {
+
+            count++;
+            Assertions.assertSame(interval, emptyStream.getInterval());
+            Assertions.assertSame(0, emptyStream.getOffset());
+
+            Consumer<Object> consumer = Mockito.mock(Consumer.class);
+            emptyStream.forEach(consumer);
+            Mockito.verify(consumer, Mockito.times(0)).accept(Mockito.anyString());
+
+            Consumer<List<Object>> listConsumer = Mockito.mock(Consumer.class);
+            emptyStream.forEachSegment(listConsumer);
+            Mockito.verify(listConsumer, Mockito.times(0)).accept(Mockito.any(List.class));
+
+            emptyStream.reset();
+            if (count == 2) {
+                emptyStream = emptyStream.map(Object::toString);
+            }
+        }
+    }
+}
---
+++ ws-springx-common/src/main/java/ws/spring/util/SegmentStreams.java
@@ -0,0 +1,138 @@
+package ws.spring.util;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * @author WindShadow
+ * @version 2025-05-28.
+ */
+public final class SegmentStreams {
+
+    public static <E> SegmentStream<E> emptySegmentStream(int interval) {
+        return new EmptySegmentStream<>(interval);
+    }
+
+    public static <E> SegmentStream<E> ofCollection(int interval, Collection<E> collection) {
+
+        List<E> els = new ArrayList<>(collection);
+        return ofSelector(interval, (offset, s) -> els.stream().skip(offset).limit(s).collect(Collectors.toList()));
+    }
+
+    public static <E> SegmentStream<E> ofSelector(int interval, SegmentStream.SegmentVernier<E> vernier) {
+
+        return new SegmentStreamImpl<>(interval, Function.identity(), vernier);
+    }
+
+    private static class EmptySegmentStream<E> extends AbstractSegmentStream<E> {
+
+        private EmptySegmentStream(int interval) {
+            super(interval);
+        }
+
+        @Override
+        public void reset() {
+        }
+
+        @Override
+        public <T> SegmentStream<T> map(int interval, Function<? super E, ? extends T> mapping) {
+            return new EmptySegmentStream<>(interval);
+        }
+
+        @Override
+        public int getOffset() {
+            return 0;
+        }
+
+        @Override
+        public List<E> nextSegment() {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public void forEach(Predicate<E> predicate) {
+        }
+
+        @Override
+        public void forEachSegment(Predicate<List<E>> predicate) {
+        }
+    }
+
+    private static class SegmentStreamImpl<S, E> extends AbstractSegmentStream<E> {
+
+        private int offset;
+        private final Function<? super S, ? extends E> mapping;
+        private final SegmentVernier<S> vernier;
+
+        private boolean ultimate;
+
+        private SegmentStreamImpl(int interval, Function<? super S, ? extends E> mapping, SegmentVernier<S> vernier) {
+            super(interval);
+            this.mapping = Objects.requireNonNull(mapping, "The mapping must not be null");
+            this.vernier = Objects.requireNonNull(vernier, "The vernier must not be null");
+            this.reset();
+        }
+
+        @Override
+        public void reset() {
+
+            this.offset = 0;
+            this.ultimate = false;
+        }
+
+        @Override
+        public <T> SegmentStream<T> map(int interval, Function<? super E, ? extends T> mapping) {
+
+            Objects.requireNonNull(mapping, "The mapping must not be null");
+            Function<? super S, ? extends T> useMapping = s -> mapping.apply(this.mapping.apply(s));
+            SegmentStreamImpl<S, T> stream = new SegmentStreamImpl<>(interval, useMapping, this.vernier);
+            stream.offset = this.offset;
+            stream.ultimate = this.ultimate;
+            return stream;
+        }
+
+        @Override
+        public int getOffset() {
+            return this.offset;
+        }
+
+        @Override
+        public List<E> nextSegment() {
+
+            List<S> segment = nextSourceSegment();
+            return segment.isEmpty() ? Collections.emptyList() : segment.stream().map(this.mapping).collect(Collectors.toList());
+        }
+
+        @Override
+        public void forEach(Predicate<E> predicate) {
+
+            Objects.requireNonNull(predicate);
+            List<S> segment;
+            while (!this.ultimate && !(segment = nextSourceSegment()).isEmpty()) {
+                for (S s : segment) {
+                    if (!predicate.test(this.mapping.apply(s))) return;
+                }
+            }
+        }
+
+        @Override
+        public void forEachSegment(Predicate<List<E>> predicate) {
+
+            Objects.requireNonNull(predicate);
+            List<E> segment;
+            while (!this.ultimate && !(segment = nextSegment()).isEmpty()) if (!predicate.test(segment)) return;
+        }
+
+        private List<S> nextSourceSegment() {
+
+            if (this.ultimate) return Collections.emptyList();
+            List<S> es = this.vernier.peek(this.offset, this.interval);
+            int size = es.size();
+            this.ultimate = size < this.interval;
+            this.offset += size;
+            return es;
+        }
+    }
+}
